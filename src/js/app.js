ed54cdc9f8d3b1b5c8c0bb656abe2e37f0d4e9ce
franky3-git <frankndounga@yahoo.fr>
2020-08-04 15:40:43 +0100

    create the constructor for question

--- src/js/app.js
+++ src/js/app.js
@@ -6,12 +6,35 @@ const actualQuestionBox = document.querySelector('.nber');
 const totalNumberQuestionBox = document.querySelector('.total-number');
 let initVar = true;
 
+//constructor for a one question
+const Question = function(question, answers, correctAnswer) {
+	this.question = question;
+	this.answers = answers;
+	this.correctAnswer = correctAnswer;
+}
+
+//set all questions
 const questions = [
-	{question: 'first question', answers: ['first', 'second', 'third', 'fourth'], answer: 'second'},
-	{question: 'second question', answers: ['first', 'second', 'third', 'fourth'], answer: 'first'},
-	{question: 'third question', answers: ['first', 'second', 'third', 'fourth'], answer: 'third'},
-	{question: 'fourth question', answers: ['first', 'second', 'third', 'fourth'], answer: 'fourth'},
-	{question: 'fifth question', answers: ['first', 'second', 'third', 'fourth'], answer: 'first'}
+	new Question(
+		'Is Javascript the best language ?',
+		['Yes', 'No'],
+		'Yes'
+	),
+	new Question(
+		'What is the fact that memory is reserved for variables and functions during the creation stage of execution context?',
+		['declaration', 'settings', 'hoisting', 'preparation'],
+		'hoisting'
+	),
+	new Question(
+		'How do you declare a constante in javascript?',
+		['var', 'const', 'let', 'constant'],
+		'const'
+	),
+	new Question(
+		'What is javascript engine use in node.js?',
+		['spidermonkey', 'chacka core', 'javascript reader', 'V8'],
+		'V8'
+	)
 ]
 
 let i = 0;
@@ -36,11 +59,11 @@ function answerQuestion(e) {
 	if(e.target.parentElement.classList.contains('answer')) {
 		
 		if(confirm('Are you sure you want to submit this answer?')){
-			if(questions[i].answer == e.target.textContent) {
+			if(questions[i].correctAnswer == e.target.textContent) {
 				score++;
 			}
 			i++;
-			//if(i >= questions.length) return;
+			log(score)
 		}
 	}
 }
@@ -53,7 +76,6 @@ answersBox.addEventListener('click', function(e) {
 			showQuestion();
 		}
 	}
-	
 });
 
 showQuestion();
