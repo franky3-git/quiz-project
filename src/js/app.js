7f1674196f946910832c01c6f9431ca967d7cce8
franky3-git <frankndounga@yahoo.fr>
2020-08-06 14:31:46 +0100

    implement more object oriented programming to the app

--- src/js/app.js
+++ src/js/app.js
@@ -1,86 +1,112 @@
 //"use strict"
-const log = console.log;
-const question = document.querySelector('.question');
-const answersBox = document.querySelector('.answers-box');
-const actualQuestionBox = document.querySelector('.nber');
-const totalNumberQuestionBox = document.querySelector('.total-number');
-let initVar = true;
+//create a sandBox
+(function() {
+	const log = console.log;
+	const question = document.querySelector('.question');
+	const answersBox = document.querySelector('.answers-box');
+	const actualQuestionBox = document.querySelector('.nber');
+	const totalNumberQuestionBox = document.querySelector('.total-number');
+	let initVar = true;
+	let i = 0;
 
-//constructor for a one question
-const Question = function(question, answers, correctAnswer) {
-	this.question = question;
-	this.answers = answers;
-	this.correctAnswer = correctAnswer;
-}
+	//constructor for a one question
+	const Question = function(question, answers, correctAnswer) {
+		this.question = question;
+		this.answers = answers;
+		this.correctAnswer = correctAnswer;
+	}
 
-//set all questions
-const questions = [
-	new Question(
-		'Is Javascript the best language ?',
-		['Yes', 'No'],
-		'Yes'
-	),
-	new Question(
-		'What is the fact that memory is reserved for variables and functions during the creation stage of execution context?',
-		['declaration', 'settings', 'hoisting', 'preparation'],
-		'hoisting'
-	),
-	new Question(
-		'How do you declare a constante in javascript?',
-		['var', 'const', 'let', 'constant'],
-		'const'
-	),
-	new Question(
-		'What is javascript engine use in node.js?',
-		['spidermonkey', 'chacka core', 'javascript reader', 'V8'],
-		'V8'
-	)
-]
 
-let i = 0;
-let score = 0;
-function showQuestion() {
-	question.textContent = questions[i].question;
-	actualQuestionBox.textContent = i + 1;
-	totalNumberQuestionBox.textContent = questions.length;
-	answersBox.innerHTML = '';
-	questions[i].answers.forEach(answer => {
-		const div = document.createElement('div');
-		const p = document.createElement('p');
-		p.textContent = answer;
-		div.classList.add('answer');
-		div.appendChild(p);
-		answersBox.appendChild(div);
-	})
-}
-
-function answerQuestion(e) {
+	Question.prototype.displayQuestion = function() {
+		question.textContent = this.question;
+		answersBox.innerHTML = '';
+		this.answers.forEach(answer => {
+			const div = document.createElement('div');
+			const p = document.createElement('p');
+			p.textContent = answer;
+			div.classList.add('answer');
+			div.appendChild(p);
+			answersBox.appendChild(div);
+		});
+	};
 	
-	if(e.target.parentElement.classList.contains('answer')) {
-		
-		if(confirm('Are you sure you want to submit this answer?')){
-			if(questions[i].correctAnswer == e.target.textContent) {
-				score++;
+	Question.prototype.checkAnswer = function(ans) {
+		let score;
+		if(ans == this.correctAnswer) {
+			score = sc(true);
+			} else {
+			score = sc(false)
+		}
+		return score;
+	};
+
+	function calcScore() {
+		let score = 0;
+
+		return function(option){
+			if(option) {
+				score++
 			}
-			i++;
-			log(score)
-		}
+			return score
+		};
 	}
-}
 
+	let sc = calcScore();
 
-answersBox.addEventListener('click', function(e) {
-	if(i < questions.length) {
-		answerQuestion(e);
-		if(i != questions.length) {
-			showQuestion();
+	//set all questions
+	const questions = [
+		new Question(
+			'Is Javascript the best language ?',
+			['Yes', 'No'],
+			'Yes'
+		),
+		new Question(
+			'What is the fact that memory is reserved for variables and functions during the creation stage of execution context?',
+			['declaration', 'settings', 'hoisting', 'preparation'],
+			'hoisting'
+		),
+		new Question(
+			'How do you declare a constante in javascript?',
+			['var', 'const', 'let', 'constant'],
+			'const'
+		),
+		new Question(
+			'What is javascript engine use in node.js?',
+			['spidermonkey', 'chacka core', 'javascript reader', 'V8'],
+			'V8'
+		)
+	];
+
+	function next() {
+		if(i >= 0 && i < questions.length) {
+			questions[i].displayQuestion();
+		} else {
+			initVar = false;
 		}
+		displayQuestionNumber();
 	}
-});
+	
+	function displayQuestionNumber() {
+		actualQuestionBox.textContent = i + 1;
+		totalNumberQuestionBox.textContent = questions.length;
+	}
 
-showQuestion();
+	next();
 
-
+	answersBox.addEventListener('click', function(e) {
+		if(initVar) {
+			if(confirm('Are you sure you want to submit this answer?')) {
+				const current = questions[i];
+				if(e.target.parentElement.classList.contains('answer')) {
+					var score = current.checkAnswer(e.target.textContent);
+				}
+				log(score);
+				i++;
+				next();
+			}	
+		}
+	});
+})();
 
 
 
